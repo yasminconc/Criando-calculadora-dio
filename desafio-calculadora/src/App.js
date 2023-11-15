c87ce3b2ec4f38fa5bc992942c27297848ab6e4a
yasminconc <yasminsilvaconc@gmail.com>
2023-11-15 16:56:42 -0300

    calculadora

---
+++ desafio-calculadora/src/App.js
@@ -0,0 +1,98 @@
+
+import Input from './components/Input';
+import Button from './components/Button';
+
+import { Container, Content, Row } from './styles';
+import { useState } from 'react';
+
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
+  };
+
+  const handleAddNumber = (num) => {
+    setCurrentNumber((prev) => (prev === '0' ? '' : prev) + num);
+  };
+
+  const handleMathOperation = (operator) => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation(operator);
+    } else {
+      const result = performOperation(Number(firstNumber), Number(currentNumber), operator);
+      setCurrentNumber(String(result));
+      setCurrentNumber('0');
+      setOperation(operator);
+    }
+  };
+ 
+
+  const performOperation = (num1, num2, operator) => {
+    switch (operator) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        return num1 / num2;
+      default:
+        return 0;
+    }
+  };
+
+
+  const handleEquals = () => {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      const result = performOperation(Number(firstNumber), Number(currentNumber), operation);
+      setCurrentNumber(String(result));
+      setOperation('');
+      setFirstNumber('0');
+    }
+  };
+
+
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="x" onClick={() => handleMathOperation('*')} />
+          <Button label="/" onClick={() => handleMathOperation('/')}/>
+          <Button label="c" onClick={handleOnClear}/>
+          <Button label="." onClick={() => handleAddNumber('.')}/>
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')}/>
+          <Button label="8" onClick={() => handleAddNumber('8')}/>
+          <Button label="9" onClick={() => handleAddNumber('9')}/>
+          <Button label="-" onClick={() => handleMathOperation('-')}/>
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')}/>
+          <Button label="5" onClick={() => handleAddNumber('5')}/>
+          <Button label="6" onClick={() => handleAddNumber('6')}/>
+          <Button label="+" onClick={() => handleMathOperation('+')}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="=" onClick={handleEquals}/>
+        </Row>
+        
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
